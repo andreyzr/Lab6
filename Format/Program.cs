caa64c295561a5501738755e63953e805271b572
user <user@WIN-36EQDOQCKT4>
2023-05-04 20:47:39 +0300

    Добавьте файлы проекта.

---
+++ Format/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Format
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultString = String.Format("{0:N2}", -5);
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string starString =Console.ReadLine();
+            string [] stringArray = starString.Split();
+            string resultString = "";
+            foreach (string s in stringArray)
+            {
+                resultString += s.Substring(0, 1).ToUpper() + s.Substring(1) + ' ';
+            }
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
